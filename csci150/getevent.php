b64eec948ea5c138e45307eff49bb110afc07a58
taniaks <35944188+taniaks@users.noreply.github.com>
2019-12-04 16:19:27 -0800

    Properly grabbing info and fixing date fromat

--- csci150/getevent.php
+++ csci150/getevent.php
@@ -6,7 +6,6 @@ if(isset($_SERVER['HTTP_ORIGIN']))
 	header('Access-Control-Allow-Credentials: true');
 	header('Access-Control-Max-Age: 86400');
 }
-
 if($_SERVER['REQUEST_METHOD']=='OPTIONS')
 {
 	if(isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
@@ -15,7 +14,6 @@ if($_SERVER['REQUEST_METHOD']=='OPTIONS')
 		header("Access-Control-Allow-Headers:		{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
 	exit(0);
 }
-
 require "dbconnect.php";
 $data = file_get_contents("php://input");
 $student_id;
@@ -23,9 +21,6 @@ $event_name;
 $event_desc;
 $start_time;
 $end_time;
-$stuff=[];
-
-
 if(isset($data))
 {
 	$request = json_decode($data,true);
@@ -34,70 +29,54 @@ if(isset($data))
 	$event_desc = $request["event_desc"];
 	$start_time= $request["start_time"];
 	$end_time= $request["end_time"];
-
 }
-
 //Turn to readable string
 $student_id = mysqli_real_escape_string($con,$student_id);
 $event_name = mysqli_real_escape_string($con,$event_name);
 $event_desc = mysqli_real_escape_string($con,$event_desc);
 $start_time = mysqli_real_escape_string($con, $start_time);
 $end_time = mysqli_real_escape_string($con, $end_time);
-
 //strip slashes of string
 $student_id=stripslashes($student_id);
-$event_name = stripslashes($con,$event_name);
-$event_desc = stripslashes($con,$event_desc);
-$start_time = stripslashes($con, $start_time);
-$end_time = stripslashes($con, $end_time);
-
-
-$sql = "SELECT event_name, event_desc, strat_time, end_time FROM event WHERE student_id = '$student_id'";
+$event_name = stripslashes($event_name);
+$event_desc = stripslashes($event_desc);
+$start_time = stripslashes($start_time);
+$end_time = stripslashes($end_time);
+$sql = "SELECT event_name, event_desc, start_time, end_time FROM event WHERE student_id = '200100'";
 $result = mysqli_query($con,$sql);
-$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-
-$count = mysqli_num_rows($result);
-$response = [];
-if($count > 0)
+//Instead of $stuff, this is the array that saves the list of events
+$eventList = [];
+//This will search the entire Table of 'event' and push each row of content into evenList
+//Each element of $eventList[0,1,2...] will contain the sql query entry with respect to the table column name (event_name, start_time, etc)
+while($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
 {
-	foreach($row as $student_id)
-	{
-		if($student_id >= 510100)
-		{
-			array_push($stuff, $event_name,$event_desc, $start_time, $end_time);
-		}
-	}
+	array_push($eventList, $row);
+
+}
+for($i=0; $i<sizeof($eventList); $i++){
+	$time= strtotime($eventList[$i]["start_time"]);
+	$start_time= date("m/d/Y h:i A", $time);
+	$eventList[$i]["start_time"]= $start_time;
+
+	$etime= strtotime($eventList[$i]["end_time"]);
+	$end_time= date("m/d/Y h:i A", $etime);
+	$eventList[$i]["end_time"]= $end_time;
+}
+
+$response = [];
+if(sizeof($eventList) > 0)
+{
+	//print_r($eventList);
+	$response[0] = "Get event was Success!";
+	//$response[1] = $eventList
+	//$eventList[0] = Array ( [event_name] => Final Exam [event_desc] => Computer science final exam! Need to study! [start_time] => 2019-11-21 10:00:00 [end_time] => 2019-11-21 12:00:00 )
+	//Can now call by key, for example: $eventList[0]['event_name'] => 'Final Exam'.
+	array_push($response, $eventList);
 }
 else
 {
-	$response = "No such student exists";
+	$response[0] = "No such student exists <br>".$con->error;
 }
 
-	//$result = mysqli_query($con,$sql);
-	//$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-	//$count = mysqli_num_rows($result);
-	for($i=0; $i<sizeof($stuff); $i++)
-	{
-		$sql = "SELECT event_name, event_desc, strat_time, end_time FROM event WHERE student_id = '$student_id'";
-		$result = mysqli_query($con,$sql);
-		$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-		$count = mysqli_num_rows($result);
-		if($count > 0)
-		{
-			$event_name[$stuff[$i]] = $row['Eventname'];
-		}
-		else
-		{
-			$response = "Error in retrieving event";
-		}
-
-	}
-
-	if(sizeof($event_name) > 0)
-	{
-		$response[0] = "Get event was Succes!";
-		array_push($response, $event_name);
-	}
-
-	echo json_encode($response);
+echo json_encode($response);
 ?>
