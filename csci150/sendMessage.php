c9bd580f061401b4ace307ebc1af80e656399193
DanielJacuinde <djacuinde96@mail.fresnostate.edu>
2019-12-04 19:46:24 -0800

    databaseUpdate

--- csci150/sendMessage.php
+++ csci150/sendMessage.php
@@ -22,32 +22,45 @@ $data = file_get_contents("php://input");
 $senderID;
 $receiverID;
 $msg;
+$date;
 
 if(isset($data)){
 	$request=json_decode($data,true);
+	
 	$senderID = $request["senderID"];
 	$receiverID = $request["receiverID"];
-  $msg = $request["message"];
+	$msg = $request["message"];
 }
+
+/*$senderID = 200100;
+$receiverID = 300100;
+$msg = "TEST";*/
+date_default_timezone_set("America/Los_Angeles");
+$date = date('Y/m/d h:i:s a', time());
+echo $date;
 
 //Turn to readable string
 $senderID = mysqli_real_escape_string($con,$senderID);
 $receiverID  = mysqli_real_escape_string($con,$receiverID);
 $msg  = mysqli_real_escape_string($con,$msg);
+$date = mysqli_real_escape_string($con,$date);
+
 //strip slashes of string
 $senderID=stripslashes($senderID);
 $receiverID=stripslashes($receiverID);
 $msg=stripslashes($msg);
+$date=stripslashes($date);
 
 //$sql = "INSERT INTO `msgdata` (`msg`, `SenderID`, `ReceiverID`, `date`) VALUES ('$msg', '$senderID', '$receiverID', current_timestamp())";
 //$result = mysqli_query($con,$sql);
 
-$sql = "INSERT INTO `msgdata` (`msg`, `SenderID`, `ReceiverID`, `date`) VALUES (?,?,?,current_timestamp())";
+$sql = "INSERT INTO msgdata (`msg`, `SenderID`, `ReceiverID`, `date`) VALUES (?,?,?,?)";
 
 $response = "";
+
 if($stmt = mysqli_prepare($con, $sql))
 {
-	mysqli_stmt_bind_param($stmt, "sss", $msg,$senderID,$receiverID,$date);
+	mysqli_stmt_bind_param($stmt, "ssss", $msg,$senderID,$receiverID,$date);
 	if(mysqli_stmt_execute($stmt))
 	{
 		$response = "Message Sent!";
